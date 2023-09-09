cae86392569406be5cbc5f4aff78f508263ecec0
roman <51534566+roman-thapa@users.noreply.github.com>
2023-09-09 08:22:45 +0530

    Use iteration to write a function fibs that logs fibonacci sequence

--- Assignment-1/fibonacci.js
+++ Assignment-1/fibonacci.js
@@ -1,1 +1,23 @@
-console.log("linked")
+let userInput = prompt("Please enter a number", 1)
+
+function fibs(num) {
+    let fibsArr = []
+    for (let i=0; i<num; i++) {
+        if(i===0) {
+            fibsArr.push(0)
+        }else if(i===1) {
+            fibsArr.push(1)
+        } else {
+            fibsArr.push(fibsArr[i-1] + fibsArr[i-2])
+        }
+        
+    //   (i===0) ?
+        //   fibsArr.push(0) :
+        //   (i===1) ?
+            //   fibsArr.push(1) :
+            //   fibsArr.push(fibsArr[i-1] + fibsArr[i-2])
+    }
+    return fibsArr
+}
+
+console.log(fibs(userInput))
