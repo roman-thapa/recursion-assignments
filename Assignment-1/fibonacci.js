b095b87ad419fcf9829fe44264e77b2cf9836767
roman <51534566+roman-thapa@users.noreply.github.com>
2023-09-09 10:25:22 +0530

    Solve the problem using Recursion and display it in the webpage

--- Assignment-1/fibonacci.js
+++ Assignment-1/fibonacci.js
@@ -1,4 +1,17 @@
-let userInput = prompt("Please enter a number", 1)
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const display =  document.querySelector('p')
+
+button.addEventListener('click', () => {
+    console.log(input.value)
+    if (input.value === '') {
+       alert("Enter a number")
+    } else {
+        display.innerText = 
+        `Using Iteration: ${fibs(input.value)}
+        Using Recursion: ${fibsRev(input.value)}`
+    }
+})
 
 function fibs(num) {
     let fibsArr = []
@@ -10,14 +23,16 @@ function fibs(num) {
         } else {
             fibsArr.push(fibsArr[i-1] + fibsArr[i-2])
         }
-        
-    //   (i===0) ?
-        //   fibsArr.push(0) :
-        //   (i===1) ?
-            //   fibsArr.push(1) :
-            //   fibsArr.push(fibsArr[i-1] + fibsArr[i-2])
     }
     return fibsArr
 }
 
-console.log(fibs(userInput))
+function fibsRev(num) {
+  if (num === 2) return [0, 1];
+  if (num ===1) return [0];
+  if (num<=0) return [];
+    return [...fibsRev(num - 1)].concat(
+      fibsRev(num - 1)[(fibsRev(num - 1)).length-1] +
+      fibsRev(num - 1)[(fibsRev(num - 1)).length-2]
+      );
+}
