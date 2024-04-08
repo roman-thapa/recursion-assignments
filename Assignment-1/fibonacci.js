1312dc18c2d16497db02d5dd3839aa75f8f6ed5f
roman <51534566+roman-thapa@users.noreply.github.com>
2024-04-08 11:44:19 +0530

    Fix fibsRev function for generating Fibonacci sequence recursively and add min input value in HTML

--- Assignment-1/fibonacci.js
+++ Assignment-1/fibonacci.js
@@ -3,7 +3,6 @@ const button = document.querySelector('button')
 const display =  document.querySelector('p')
 
 button.addEventListener('click', () => {
-    console.log(input.value)
     if (input.value === '') {
        alert("Enter a number")
     } else {
@@ -28,12 +27,18 @@ function fibs(num) {
 }
 
 function fibsRev(num) {
+    if (num <= 0) return [];
     if (num === 1) return [0];
     if (num === 2) return [0, 1];
-    if (num <= 0) return [];
-    
+  
     const prevSequence = fibsRev(num - 1);
-    const nextNumber = prevSequence[prevSequence.length - 1] + prevSequence[prevSequence.length - 2];
+    const length = prevSequence.length;
+    
+    if (length < 2) {
+      return prevSequence.concat(length === 0 ? 0 : 1);
+    }
+  
+    const nextNumber = prevSequence[length - 1] + prevSequence[length - 2];
     
     return [...prevSequence, nextNumber];
   }
